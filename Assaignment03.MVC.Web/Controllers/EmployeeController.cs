bfb473b649819c08ba3196b415ab13aa937aaf32
Noran <Noranmedht63@gmail.com>
2024-09-08 01:53:31 +0300

    Add project files.

---
+++ Assaignment03.MVC.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,84 @@
+﻿using Assignment.Service.Interfaces.Department;
+using Assignment.Service.Interfaces.Employee;
+using Assignment.Service.Interfaces.Employee.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment05.MVC.Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeServices _employeeServices;
+        private readonly IDepartmentService _departmentService;
+
+        public EmployeeController(IEmployeeServices employeeServices,IDepartmentService departmentService)
+        {
+            _employeeServices = employeeServices;
+            _departmentService = departmentService;
+        }
+        
+        public IActionResult Index(string SearchInp)
+        {
+            IEnumerable<EmployeeDto> employees=new List<EmployeeDto>();
+            if (string.IsNullOrEmpty(SearchInp))
+                 employees = _employeeServices.GetAll();
+           
+            else 
+                 employees=_employeeServices.GetEmployeeByName(SearchInp);
+           
+            return View(employees);
+
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Departments = _departmentService.GetAll();
+            return View();
+        }
+        
+
+
+        [HttpPost]
+        public IActionResult Create(EmployeeDto employee)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _employeeServices.Add(employee);
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(employee);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Employee Error", ex.Message);
+                return View(employee);
+            }
+        }
+
+        //public IActionResult Details()
+        //{
+
+
+
+        //}
+        //[HttpGet]
+        //public IActionResult Update()
+        //{
+
+        //}
+        //[HttpPost]
+        //public IActionResult Update()
+        //{
+
+
+        //}
+
+        //public IActionResult Delete()
+        //{
+
+
+
+        //}
+    }
+}
---
+++ Assignment.Data/Contexts/AssignmentDBContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment.Data.Contexts
+{
+    public class AssignmentDBContext : DbContext
+    {
+        public AssignmentDBContext(DbContextOptions options) : base(options)
+        {
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("");
+        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            //modelBuilder.Entity<BaseEntity>().HasQueryFilter(X => !X.IsDeleted);
+            base.OnModelCreating(modelBuilder);
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
